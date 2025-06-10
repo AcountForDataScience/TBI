162916f3caf5c89102b28b6cec158f35cd4b26b7
Aram <139945779+AcountForDataScience@users.noreply.github.com>
2025-06-10 23:33:18 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import telebot
+import numpy as np
+import pandas as pd
+import random
+from sklearn import linear_model
+from sklearn.linear_model import LogisticRegression
+from sklearn import linear_model
+from scipy import stats
+from telebot import types
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import io
+
+import heapq
+
+import csv
+
+from lifelines.utils import concordance_index
+from lifelines import CoxPHFitter
+from lifelines.utils import concordance_index as cindex
