7be5d6c75ea2c285ffffa2c22f7151909e777e76
Aram <139945779+AcountForDataScience@users.noreply.github.com>
2025-06-11 06:37:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -406,7 +406,7 @@ def recommend_best_treatment(patient_data: dict):
 
   # Тепер ви можете використовувати навчену модель для прогнозування ускладнень для нових пацієнтів
   # Наприклад, для нового пацієнта з такими характеристиками:
-  NewPatient = pd.DataFrame({
+NewPatient = pd.DataFrame({
    'Initial_GCS': [x1],
    'Age': [x2],
    'Hematoma_size_ml': [x3],
@@ -418,16 +418,16 @@ def recommend_best_treatment(patient_data: dict):
 
   #Resume_predicted_proba_lr_Cognitive_Disorders = Resume_predicted_proba_lr_Cognitive_Disorders[-1][1]
 
-  NeurologicalOutcomeProbability = model.predict(NewPatient)
-  if NeurologicalOutcomeProbability < 1:
+NeurologicalOutcomeProbability = model.predict(NewPatient)
+ if NeurologicalOutcomeProbability < 1:
     NeurologicalOutcomeProbabilityAnswer = ': invalidity'
-  else:
+ else:
     NeurologicalOutcomeProbabilityAnswer = ': significant recovery'
-  NeurologicalOutcomeProbabilityPercent = model.predict_proba(NewPatient)
-  NeurologicalOutcomeProbabilityPercent = NeurologicalOutcomeProbabilityPercent[-1][1]
-  NeurologicalOutcomeProbabilityPercent = NeurologicalOutcomeProbabilityPercent*100
+ NeurologicalOutcomeProbabilityPercent = model.predict_proba(NewPatient)
+ NeurologicalOutcomeProbabilityPercent = NeurologicalOutcomeProbabilityPercent[-1][1]
+ NeurologicalOutcomeProbabilityPercent = NeurologicalOutcomeProbabilityPercent*100
 
-  features = ['Initial_GCS',
+ features = ['Initial_GCS',
    'Age',
    'Hematoma_size_ml',
    'Midline_shift_mm',
@@ -435,13 +435,13 @@ def recommend_best_treatment(patient_data: dict):
    'ICP_max_to_surgery',
    'Concomitant_traumas']
   # Важливість ознак
-  importances = model.feature_importances_
-  feature_importance_df = pd.DataFrame({'Feature': features, 'Importance': importances})
-  feature_importance_df = feature_importance_df.sort_values(by='Importance', ascending=False)
-  feature_importance_dict = feature_importance_df.set_index('Feature')['Importance'].to_dict()
+ importances = model.feature_importances_
+ feature_importance_df = pd.DataFrame({'Feature': features, 'Importance': importances})
+ feature_importance_df = feature_importance_df.sort_values(by='Importance', ascending=False)
+ feature_importance_dict = feature_importance_df.set_index('Feature')['Importance'].to_dict()
 
 
-  return feature_importance_dict, NeurologicalOutcomeProbabilityAnswer, NeurologicalOutcomeProbabilityPercent
+ return feature_importance_dict, NeurologicalOutcomeProbabilityAnswer, NeurologicalOutcomeProbabilityPercent
 
 
 def Calculate_CGS(x1, x2, x3):
